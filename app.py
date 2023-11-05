9de7cfc6e7317aac03c8a23f22b6b5310c6474bf
markagugo <73078814+markagugo@users.noreply.github.com>
2023-11-05 15:36:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import joblib
 import streamlit as st
+from sklearn.exceptions import InconsistentVersionWarning
+import warnings
 
-with open('model_and_vectorizer.pkl', 'rb') as model_file:
-    loaded_model, loaded_cv = joblib.load(model_file)
+# Suppress the warning and capture the original scikit-learn version
+warnings.simplefilter("error", InconsistentVersionWarning)
+
+try:
+    with open('model_and_vectorizer.pkl', 'rb') as model_file:
+        loaded_model, loaded_cv = joblib.load(model_file)
+except InconsistentVersionWarning as w:
+    original_version = w.original_sklearn_version
+    st.error(f"Original scikit-learn version: {original_version}")
+    st.stop()
 
 post_text = st.text_input('Enter Sentence')
 
