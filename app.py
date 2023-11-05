3c67b9b654ec00351015c32261bb0ef8dab97ee6
markagugo <73078814+markagugo@users.noreply.github.com>
2023-11-05 15:51:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ import streamlit as st
 with open('model_and_vectorizer.pkl', 'rb') as model_file:
     loaded_model, loaded_cv = joblib.load(model_file)
 
-post_text = st.text_input('Enter Sentence')
+post_text = st.text_input('post_input')
 
-if st.button('Check'):
-    # Transform the input text using the loaded CountVectorizer
+if st.button('check'):
     text_array = loaded_cv.transform([post_text]).toarray()
 
-    # Predict using the loaded model
     prediction = loaded_model.predict(text_array)
 
-    # Display the prediction
     st.text(prediction)
