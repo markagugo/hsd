cd4364e037e0664aeed62210bb921877219d6d8c
markagugo <73078814+markagugo@users.noreply.github.com>
2023-11-05 14:24:53 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import pickle
+import streamlit as st
+
+# Load both the model and CountVectorizer
+with open('model_and_vectorizer.pkl', 'rb') as model_file:
+    loaded_model, loaded_cv = pickle.load(model_file)
+
+post_text = st.text_input('Enter Sentence')
+
+if st.button('Check'):
+    # Transform the input text using the loaded CountVectorizer
+    text_array = loaded_cv.transform([post_text]).toarray()
+
+    # Predict using the loaded model
+    prediction = loaded_model.predict(text_array)
+
+    # Display the prediction
+    st.text(prediction)
